bcba7276ee0a0cef9d0ddaf2241cd907e703da12
nicolaipinto422-commits <nicolaipinto422@gmail.com>
2026-09-24 16:24:15 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="elit-pet-grooming-2.0-v1";
+const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.json","./logo.jpg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
